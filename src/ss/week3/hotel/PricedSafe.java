100f0ab7c1006116be295e4038cedbd6ed92e0b8
2617730 <h.hamielec@student.utwente.nl>
2021-12-06 11:04:05 +0100

    finished PricedSafe (1.10)

--- src/ss/week3/hotel/PricedSafe.java
+++ src/ss/week3/hotel/PricedSafe.java
@@ -1,36 +1,88 @@
 package ss.week3.hotel;
 
+import ss.week3.bill.Bill;
 import ss.week3.password.Password;
 
-public class PricedSafe extends Safe {
+public class PricedSafe extends Safe implements Bill.Item{
 
     private Password password;
     private Safe Safe;
-    //activate: receives a String with a password text as a parameter, ------
-    // activates the safe if password is correct; -----
-    //• activate: without parameters, overrides the parent method, gives a warning and does not activate
-    //the safe;
+    public double price;
+
+    public PricedSafe(double price){
+        this.price = price;
+        this.password = new Password();
+    }
+
+    /**
+     * activates the safe if received password is correct;
+     * @param Password - provided password
+     */
     public void activate(String Password){
-        String actualPassword = password.getPassword(); // password of the safe
-        if(Password.equals(actualPassword)){
+        if(password.testWord(Password)){
             Safe.activate();
         }
     }
 
-    //• deactivate: without parameters, closes the safe and deactivates it;
+    /**
+     * overrides the parent method,
+     * gives a warning and does not activate the safe;
+     */
+    @Override
+    public void activate(){
+        System.out.println("WARNING!");
+    }
 
+    /**
+     * closes the safe
+     * and deactivates it
+     */
+    public void deactivate(){
+        Safe.close();
+        Safe.deactivate();
 
+    }
 
-    //• open: receives a String with a password text as a parameter,
-    // opens the safe if it is active, and password is correct;
-    //• open: without parameters, overrides the parent method and does not change the state of the safe;
+    /**
+     * opens the safe if it's active & password is correct
+     * @param Password - given password
+     */
+    public void open(String Password){
+        if (Safe.isActive() && password.testWord(Password)){
+            Safe.open();
+        }
+    }
 
+    /**
+     * overrides the parent method and does not change the state of the safe;
+     */
+    @Override
+    public void open(){
+        System.out.println("ERROR!");
+    }
 
-    //• close: without parameters, c
-    // loses the safe (but does not change its activation status).
+    /**
+     * closes the safe
+     * does not change its activation status
+     */
+    public void close(){
+        Safe.close();
+    }
 
+    /**
+     * return password object on which the method testWord can be called to check password
+     * @return password
+     */
+    public Password getPassword(){
+        return password;
+    }
 
-    //getPassword:
-    // returns the password object on which the method testWord can be called to check password
-
+    /**
+     * @return amount of this Item
+     * @ensures: result >=0;
+     */
+    @Override
+    public double getAmount() {
+        return price;
+    }
 }
