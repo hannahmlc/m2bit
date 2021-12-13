abc1242d4b7c43c97f90e3e3855500cf9b57eb51
2617730 <h.hamielec@student.utwente.nl>
2021-12-13 15:44:09 +0100

    4.11 done

--- src/ss/week4/MapUtil.java
+++ src/ss/week4/MapUtil.java
@@ -10,17 +10,29 @@ public class MapUtil {
      * checks whatever Map<K,V> f passed as a parameter is an injection
      * @param map - map f
      * @param <K> - key set of map f
-     * @param <V> - elements of map f
+     * @param <V> - value set of map f
      * @return true if for all v in the value set of the map f, there exists exactly one key k in the map’s key set, such that v == f.get(k)
      */
     public static <K, V> boolean isOneOnOne(Map<K, V> map) {
         HashSet<V> valueSet = new HashSet<>(map.values());
         return (valueSet.size() == map.keySet().size());
     }
-    
+
+    /**
+     * checks whether parameter map f is surjective
+     * @param map - map f
+     * @param range - range of elements
+     * @param <K> - key set of map f
+     * @param <V> - value set of map f
+     * @return true if for all elements in range there is a key such that f maps to this element
+     */
     public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-4.11
-        return false;
+        for (int i = 0; i < range.size(); i++) {
+            if (!map.containsValue(range.toArray()[i])) {
+                return false;
+            }
+        }
+        return true;
     }
     
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
